600dda701ab831f00dd5f7a158d00daddca1c1de
Avery <abriggs1@unb.ca>
2018-11-12 02:40:19 -0400

    2018-11-12 Finished Assignment 7

--- Assignment7/addressConvert.c
+++ Assignment7/addressConvert.c
@@ -1,25 +1,120 @@
+/*
+* CS3413 Assignment 7
+* Nov 2018
+* Avery Briggs
+* 3471065
+*
+* C program to read logical memory address for a
+* 2^32 virtual space. Program converts to a physical
+* address, assuming that the page sizing is 2^12 (4 kb)
+* A mamximum number of frames that may be loaded into
+* memory at one time is given from the command line.
+* Usage: ./addressConvert # fileName.txt (optional)
+* When entering logical addresses on the command line
+* the reporting will appear to be delayed, but the
+* principal form for input should be redirection on
+* the command line to a prepared text file.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
 
+typedef int bool;
+#define true 1
+#define false 0
+
+// offsetMask - used to calculate the offset of a logical address
+// pageMask - the size of a page
+// pageFaults - running total of pagefaults
 const long int offsetMask = (pow(2,12)-1), pageMask = pow(2,12);
+int pageFaults = 0;
+
+// Function returns true or false whether an address's
+// page is already loaded.
+bool check(int table[], int page, int size){
+  for(int i = 0; i < size; i++){
+    if(table[i] == page){
+      return true;
+    }
+  }
+  return false;
+}
+
+//Function is called to insert a page into memory
+void add(int page, int table[], int size){
+  if(check(table, page, size)){
+    return;
+  }
+  for(int i = 0; i < size; i++){
+    if(table[i] < 0){
+      table[i] = page;
+      pageFaults++;
+      return;
+    }
+  }
+}
+
+// Funtion uses the FIFO algorithm to simulate
+// paging. It shuffles the pagetable array to make
+// room at the end
+void fifo(int table[], int size){
+  if(size == 1){
+    table[0] = -1;
+  }
+  else{
+    int i,j;
+    for(i = 0, j = 1; j < size; i++, j++){
+      table[i] = table[j];
+    }
+    table[i] = -1;
+  }
+}
 
 int main(int argc, char ** argv){
-  if(argc < 2 || argc > 3){
+  if(argc < 2 || argc > 3 || atoi(argv[1]) < 0 || atoi(argv[1]) > pow(2,20)){
     printf("Usage: ./addressConvert # FILENAME.txt (optional)\n");
     return EXIT_FAILURE;
   }
-  long int num, count = 0;
-  int y = 500;
+  long int num;
   char numS[32];
-  long int pageTable[(int)pow(2,20)][2];
-  while(count < 10){
-    printf("\n\n\n");
-    scanf("%ld\n", &num);
-    sprintf(numS, "%d", num);
-    printf("numS: %s, strlen(numS): %d\n", numS, strlen(numS));
-    printf("num: %ld, num/pageMask: %ld, num&offsetMask: %ld\n", num, (num/pageMask), (num&offsetMask));
-    count++;
+  int i, numFrames = atoi(argv[1]), y;
+  int numFramesInUse = 0, phPage, phOff;
+  int * pageTable;
+  pageTable = malloc(numFrames*sizeof(int));
+  for(i = 0; i < numFrames; i++){
+    pageTable[i] = -1;
   }
+  y = scanf("%ld\n", &num);
+  printf("Logical\t\t\tPhysical\n\t\t\tPage\tOffset\n");
+  while(y == 1){
+    if(num > pow(2,32)){
+      printf("Number is too large. only 2^32 memory\n");
+    }
+    else if(num < 0){
+      printf("Number is too small. only positives\n");
+    }
+    else{
+      sprintf(numS, "%ld", num);
+      phPage = num/pageMask;
+      phOff = num & offsetMask;
+      printf("%ld\t%s->\t%d\t%d\n", num, ((strlen(numS)<8)?"\t":""), phPage, phOff);
+      if(!check(pageTable, phPage, numFrames) && numFramesInUse < numFrames){
+          add(phPage, pageTable, numFrames);
+          numFramesInUse++;
+      }
+      else if(numFramesInUse == numFrames){
+        if(!check(pageTable, phPage, numFrames)){
+          fifo(pageTable, numFrames);
+          numFramesInUse--;
+          add(phPage, pageTable, numFrames);
+          numFramesInUse++;
+        }
+      }
+    }
+    y = scanf("%ld\n", &num);
+  }
+  free(pageTable);
+  printf("Total page faults: %d\n", pageFaults);
 }
--- Assignment7/formula.c
+++ Assignment7/formula.c
@@ -5,17 +5,30 @@
 int main() {
 	//code
 	const int BAR = 36864, first = 65535;
-	const long int offMask = 4095, pageMask = ((pow(2,32))-(pow(2,12)));
+	const long int offMask = 4095, pageMask = ((pow(2,32))-(pow(2,12))), val = pow(2,32);
 	printf("offMask: %ld\npageMask: %ld\n", offMask, pageMask);
-	long int num;
-	int scan = scanf("%ld", &num), count = 0;
-	while(scan == 1 && count < 10){
-			printf("METHOD 1\n");
-	    printf("num: %ld & first: %ld -> (num&first = %d) -> - BAR(%ld) = %ld\n", num, first, (num&first), BAR, ((num&first)-BAR));
-			printf("\nMETHOD 2\n");
-			printf("num: %ld / 4*1024 %ld -> %ld, num & offMask -> %ld\n", num, (4*1024), (num/(4*1024)), (num&offMask));
-			scanf("%ld", &num);
-	    count++;
+	long int num, count, temp,temp2;
+	int scan = scanf("%ld", &num);
+count = 0, temp = 0, temp2;
+printf("count: %ld\n", count);
+	while(count < pow(2,18)){
+	//while(scan == 1 && count < 10){
+			// printf("METHOD 1\n");
+	    // printf("num: %ld & first: %ld -> (num&first = %d) -> - BAR(%ld) = %ld\n", num, first, (num&first), BAR, ((num&first)-BAR));
+			// printf("\nMETHOD 2\n");
+			// printf("num: %ld / 4*1024 %ld -> %ld, num & offMask -> %ld\n", num, (4*1024), (num/(4*1024)), (num&offMask));
+			// scanf("%ld", &num);
+			printf("%ld\n", count);
+			temp++;
+			count+=4096;
+			// if(temp % 7 == 0){
+	    // 	count+=40960;
+			// 	temp == 0;
+			// 	temp2++;
+			// }
+			// if(temp2 >= 5){
+			// 	break;
+			// }
 	}
 	return 0;
 }

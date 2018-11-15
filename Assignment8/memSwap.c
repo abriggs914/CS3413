e4b73af05e0cdfcc771dcec8c87f925b19d80063
Avery Briggs <abriggs1@unb.ca>
2018-11-15 15:27:36 -0400

    2018-11-15 Can perform I/O now. Need to make use of pageTable.

--- Assignment8/memSwap.c
+++ Assignment8/memSwap.c
@@ -1,36 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 int pageFaults, swapSaves, swapLoads;
 char * algorithm;
 int numFrames;
 
-//Function is called to pull the memT and alg values from argv
-void decipher(char ** line){
+typedef int bool;
+#define true 1
+#define false 0
+
+//Function is called to pull the algorithm and numFrames values from argv
+bool decipher(char ** line){
     int i;
     for(i = 1; i < 3; i++){
-        printf("line[%d]: %s, strlen(line[1]: %d\n", i, line[1], strlen(line[i]));
         if(strlen(line[i]) == 2){
-            if(line[i][1] == 'f' ){
+          if(line[i][0] == '-'){
+            if(line[i][1] == 'f'){
                 algorithm = "first in first out";
             }
-            else if(line[i][1] == 'l' ){
+            else if(line[i][1] == 'l'){
                 algorithm = "least recently used";
             }
-        }
-        else if(strlen(line[i]) < 3){//line[i][1] == 'f'){
-            printf("%d\n", atoi(line[1]));
-            numFrames = atoi(line[i]);
+            else{
+                return false;
+            }
+          }
+          else{
+              numFrames = atoi(line[i]);
+          }
         }
     }
+    return true;
 }
+
+// char * read(char * line){
+//
+// }
 
 int main(int argc, char **argv){
     if(argc < 3 || argc > 4){ // early fail condition
         printf("Usage: ./memSwap # -(one of; -f, -l) < fileName.txt (optional)\n");
         return EXIT_FAILURE;
     }
-    decipher(argv);
+    if(!decipher(argv) || numFrames < 0 || numFrames > pow(2,32)){
+      printf("Adjust numFrames or algorithm.\nUsage: ./memSwap # -(one of; -f, -l) < fileName.txt (optional)\n");
+      return EXIT_FAILURE;
+    }
     printf("algorithm: %s, numFrames: %d\n", algorithm, numFrames);
+
+    int scan, mask = pow(2,12);
+    char action;
+    long logical = 0, physical;
+    unsigned int * pageTable;
+    pageTable = malloc(pow(2,20)*sizeof(unsigned int));
+    scan = scanf("%c%ld\n", &action, &logical);
+    while(scan == 2){
+      printf("scan: %d, action: %c, logical: %d, physical: %d\n", scan, action, logical, ((logical>>12)));
+      scan = scanf("%c %ld\n", &action, &logical);
+    }
+    return EXIT_SUCCESS;
 }
